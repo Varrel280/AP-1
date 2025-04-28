82feec481b279e22ad0b4fb844e129cd2f473323
Varrel280 <varrel1707hutabarat@gmail.com>
2025-04-28 11:15:04 +0700

    Perulangan

---
+++ pertemuan 4/loop.cpp
@@ -0,0 +1,25 @@
+// menampilkan bilangan genap 10 -> 2 dengan goto
+// int i = 10;
+// genap;
+// if(i % 2 == 0) {
+//     cout << i << endl;
+// } i --;
+
+// if(i >= 2) {
+//     goto genap;
+//  }
+
+// statement while
+// int i = 1;
+// while (i <= 10) {
+//     if (i % 2 == 0) {
+//         cout << i << " ";
+//     } i++;
+// } 
+
+// statement Do-While
+int i = 1;
+ do {
+    cout << i << endl;
+ } while (i <= 0);
+}
