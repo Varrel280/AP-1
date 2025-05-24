dfb166da5ebf8625662452040f6a5ac783defc12
Varrel280 <varrel1707hutabarat@gmail.com>
2025-05-24 12:34:04 +0700

    latihan1.cpp

--- pertemuan 4/latihan1.cpp
+++ pertemuan 4/latihan1.cpp
@@ -16,3 +16,24 @@ int main() {
 
     return 0;
 }
+
+#include <iostream>          // Digunakan untuk input dan output standar
+using namespace std;         // Agar tidak perlu menulis std:: sebelum cout/cin
+
+int main() {
+    string kalimat;          // Variabel untuk menyimpan kalimat input
+    int i;                   // Variabel indeks untuk loop
+
+    cout <<"Masukkan kalimat : ";
+    getline(cin, kalimat);   // Menerima input satu baris penuh termasuk spasi
+
+    // Loop untuk mengubah setiap karakter dalam kalimat menjadi huruf besar
+    for(i = 0; i < kalimat.length(); i++){
+        kalimat[i] = toupper(kalimat[i]);  // Fungsi toupper() mengubah huruf ke kapital
+    }
+
+    // Menampilkan hasil
+    cout << "Kalimat dalam huruf kapital : " << kalimat << endl;
+
+    return 0;  // Mengakhiri program
+}
