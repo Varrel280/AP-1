dd5f40e5c56f473ff6e0b8dfb504eba3fdfac8f5
Varrel280 <varrel1707hutabarat@gmail.com>
2025-05-24 12:58:23 +0700

    variabel.cpp

--- pertemuan6/variabel.cpp
+++ pertemuan6/variabel.cpp
@@ -22,3 +22,29 @@
      // coba akses
      // cout << namaLokal << endl; // ga iso
  }
+
+
+#include <iostream> // Library standar untuk input dan output
+using namespace std; // Agar tidak perlu menuliskan 'std::' di depan cout, string, dll
+
+string namaGlobal = "Ilmu"; // Variabel global, bisa diakses dari seluruh bagian program
+
+void namaVariabel() {
+    string namaLokal = "Komputer"; // Variabel lokal, hanya bisa diakses di dalam fungsi ini
+
+    // coba akses variabel lokal
+    cout << namaLokal << endl; // Menampilkan "Komputer"
+
+    // coba akses variabel global
+    cout << namaGlobal << endl; // Menampilkan "Ilmu"
+}
+
+int main() {
+    namaVariabel(); // Memanggil fungsi namaVariabel()
+
+    // coba akses variabel global
+    cout << namaGlobal << endl; // Menampilkan "Ilmu"
+
+    // coba akses variabel lokal
+    // cout << namaLokal << endl; // ga iso (tidak bisa diakses karena di luar scope fungsi namaVariabel)
+}
