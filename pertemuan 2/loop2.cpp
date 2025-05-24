dce6c500b0e880fe163dcf46016aee9329ae1153
Varrel280 <varrel1707hutabarat@gmail.com>
2025-05-24 09:28:12 +0700

    Penggunaan operator

---
+++ pertemuan 2/loop2.cpp
@@ -0,0 +1,71 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    system("cls");
+    // Goto Label
+    // Hello world, Fasilkom-TI, Ilmu Komputer, IKLC
+    //--> Hello World, IKLC, Ilmu Komputer, FASILKOM-TI
+
+//    /* a: 
+//        cout <<"Hello World "<<endl;
+//        goto d;
+//     b: 
+//        cout <<"Fasilkom-TI "<<endl;
+//        return 0;
+//     c: 
+//        cout <<"Ilmu Komputer "<<endl;
+//        goto b;
+//     d: 
+//        cout <<"IKLC "<<endl;
+//        goto c;*/
+
+//     //Menampilkan bilangan genap 10->2 dengan goto
+//     /*int i = 10;
+
+//     genap: 
+//     if (i % 2 == 0){
+//         cout << i <<  endl;
+//     } i--;
+
+//     if (i >= 2){
+//         goto genap;
+//     }*/
+
+//     // Statement while
+//    /* int i = 1;
+//     while (i <= 10){
+//         if (i % 2 == 0){
+//             cout << i << " ";
+//         } i++;
+//     }*/
+
+//     // Statement D0-While
+//     int i = 1 ;
+//     do{
+//         cout << i << endl;
+//     }while (i <= 0);
+
+// statement for
+// for (inisialisasi, kondisi, increase)
+// for (int i = 1; i <= 10; i+=2) {
+//     cout << "Hello world" << endl;
+// }
+
+// nested for
+// ***** 5 x 5
+// for (int i = 1; i <= 5; i++) {
+//      for(int j = 1; j <= 5; j++) {
+//         cout << " * ";
+//      }
+//      cout << endl;
+//     }  
+
+    // segitiga siku-siku
+//     for(int i =1; i <=5; i++) {
+//         cout << "* ";
+//     }
+//     cout << endl;
+//   }
+}
