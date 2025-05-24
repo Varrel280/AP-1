ec7bff09347b219d0ffbd25c00bfa0aac612dd4f
Varrel280 <varrel1707hutabarat@gmail.com>
2025-05-24 12:57:04 +0700

    headerCustom.h

--- pertemuan6/headeCustom.h
+++ pertemuan6/headeCustom.h
@@ -2,3 +2,8 @@
  #define l <<
  #define o cout
  #define e endl;
+
+#define u using namespace std; // Mendefinisikan makro 'u' yang akan digantikan dengan 'using namespace std;' untuk menyederhanakan penulisan.
+#define l <<                    // Mendefinisikan makro 'l' sebagai pengganti operator '<<' (insertion operator pada cout).
+#define o cout                 // Mendefinisikan makro 'o' sebagai pengganti 'cout', digunakan untuk menampilkan output ke layar.
+#define e endl;                // Mendefinisikan makro 'e' sebagai pengganti 'endl;', untuk mencetak baris baru dan mengosongkan buffer output.
